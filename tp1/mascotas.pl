b128cc5a2a50a182a894b80cff398bbe579d9574
adiego-101 <adiego.portillo@gmail.com>
2025-08-25 21:47:42 +0000

    add mascotas.pl

---
+++ tp1/mascotas.pl
@@ -0,0 +1,9 @@
+mascotas(perro, fido).
+mascotas(gato,whiskers).
+mascota(perro,rex).
+dueño(juan,fido).
+dueño(ana,whiskers).
+dueño(pedro,rex).
+color(marron,fido).
+color(whiskers,gris).
+color(rex,negro).
